1d0b853f236837ff977f06a3f960dbf911a6375b
gothwavy <ryanalmeidadev@gmail.com>
2024-05-14 04:36:16 -0300

    Terceira section (falta finalizar)

--- src/components/Card/Card1.jsx
+++ src/components/Card/Card1.jsx
@@ -1,7 +1,17 @@
+import { useState } from "react"
+
 export default function Card1(props) {
-  return(
-    <div className="flex justify-center items-center border-2 bg-white border-black px-16 py-52 rounded-[1rem] drop-shadow-[0_10px_4px_rgba(0,0,0,0.25)]">
-      <img src={props.icone} alt="" className="size-28" />
+  const [imagem, imagemAtualizada] = useState(props.icone);
+  const imagemRGB = props.iconeRGB
+  const imagemPR = props.icone
+
+  return (
+    <div onMouseEnter={() => imagemAtualizada(imagemRGB)} onMouseLeave={() => imagemAtualizada(imagemPR)} className="group transition-all ease-in-out duration-1000 p-1 bg-white drop-shadow-[0_10px_4px_rgba(0,0,0,0.25)] rounded-[1rem] hover:p-1 hover:bg-gradient-to-br from-rosa from-0% via-roxo via-40% to-ciano to-100%">
+      <div className="flex flex-col gap-4 justify-center items-center bg-white w-64 h-[26rem] rounded-[calc(1rem-0.25rem)] transition-all ease-in-out duration-300 hover:w-96">
+        <img src={imagem} />
+        <p className="hidden text-center w-3/4 group-hover:block"><span className="text-xl bg-gradient-to-br from-rosa from-0% via-roxo via-40% to-ciano to-100% text-transparent bg-clip-text">{props.titulo}</span> <br />
+        {props.desc}</p>
+      </div>
     </div>
   )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -14,6 +14,9 @@ import Linkedin from "../../assets/icons/linkedin.svg";
 import Calculator from "../../assets/icons/Calculator.svg";
 import Calendar from "../../assets/icons/Calendar.svg";
 import Graph from "../../assets/icons/Graph.svg";
+import GraphRGB from "../../assets/icons/GraphGradiente.svg";
+import CalendarRGB from "../../assets/icons/CalendarGradiente.svg";
+import CalculatorRGB from "../../assets/icons/CalculatorGradiente.svg";
 
 export default function Home() {
   return (
@@ -112,13 +115,13 @@ export default function Home() {
                 Principais funcionalidades!
               </h2>
             </div>
-            <div className="w-screen flex flex-row justify-center gap-10">
-              <Card1 icone={Calendar}/>
-              <Card1 icone={Graph}/>
-              <Card1 icone={Calculator}/>
+            <div className="flex flex-row justify-center gap-10">
+              <Card1 icone={Calendar} iconeRGB={CalendarRGB} titulo="Calendario inteligente" desc="Datas, vendas e prazos importantes, nos te avisamos!"/>
+              <Card1 icone={Graph} iconeRGB={GraphRGB} titulo="Metricas e desempenho" desc="Acompanhe seu progresso, gastos, vendas e lucro!"/>
+              <Card1 icone={Calculator} iconeRGB={CalculatorRGB} titulo="Calculadora de custos" desc="Calcule seus gastos e precifique melhor os seus serviços e/ou seu negocio!"/>
             </div>
             <div>
-              <h2 className="size-fit text-[2.25rem] ">
+              <h2 className="w-screen text-[2.25rem] text-center">
                 Reunimos tudo que voce precisa em um so lugar<span className="bg-gradient-to-b from-rosa from-0% via-roxo via-40% to-ciano to-100% text-transparent bg-clip-text">!</span>
               </h2>
               <p className="text-center font-poppins-regular mt-4 text-lg">Cansado de usar diversas ferramentas diferentes somente para um proposito? <br />
